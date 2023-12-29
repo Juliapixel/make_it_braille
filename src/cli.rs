cdc2006c1c379265c2475699aee85f838048a010
Juliapixel <89038897+Juliapixel@users.noreply.github.com>
2023-12-29 15:02:52 -0300

    add descriptions to arguments in --help

--- src/cli.rs
+++ src/cli.rs
@@ -3,19 +3,27 @@ use clap::{Parser, ValueEnum};
 #[derive(Debug, Parser)]
 #[command(author, version, about)]
 pub struct Args {
-    #[arg(long)]
+    #[arg(long, long_help, help = "width in dots of the output image")]
+    /// width in dots of the output image, defaults to 64, keeps aspect ratio if
+    /// only height is defined instead
     pub width: Option<u32>,
 
-    #[arg(long)]
+    #[arg(long, long_help, help = "height in dots of output image")]
+    /// height in dots of the output image, keeps aspect ratio if not defined
     pub height: Option<u32>,
 
-    #[arg(long, default_value = "sierra2")]
+    #[arg(long, long_help, default_value = "sierra2", help = "dithering algorithm to use")]
+    /// dithering algorithm to use, defaults to the Sierra two-row algorithm
     pub dithering: DitheringOption,
 
-    #[arg(long)]
+    #[arg(long, long_help, help = "allow blank braille characters")]
+    /// allows blank braille characters, instead of replacing them with a single dot,
+    /// which can cause images to appear skewed, especially on windows, even with
+    /// a monospace font.
     pub allow_blank_chars: bool,
 
     #[arg(long)]
+    /// invert dots, making light values in the source image be raised dots instead
     pub invert: bool,
 
     #[arg(long, default_value = "1.0")]
@@ -24,9 +32,11 @@ pub struct Args {
     #[arg(long, default_value = "1")]
     pub brighten: i32,
 
-    #[arg(short, long, action = clap::ArgAction::Count)]
+    #[arg(short, action = clap::ArgAction::Count)]
+    /// -v to see INFO logging, -vv to see DEBUG logging
     pub verbose: u8,
 
+    /// either the path to a local image file or an http(s) URL to one
     pub file: String
 }
 
